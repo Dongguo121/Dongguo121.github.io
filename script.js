b9434329828c2c2ebf5678ecc2923697f766b57b
Dongguo121 <121589202+Dongguo121@users.noreply.github.com>
2024-06-29 03:50:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function Hello() {
-   alert("Hello, World");
-}
 const inputs = document.getElementById("my-form").elements;
 const inputByIndex = inputs[0];
 const inputByName = inputs["username"];
+function Hello() {
+   alert("Hello, World"+inputByName);
+}
